0f2bf7136db96b1a8da4deb5bf45c26c235029cb
edwinfr <edwinfrcperez@gmail.com>
2021-07-01 21:33:13 -0600

    v10.3

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include("database_connection.php");?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>  
-    <body>  
+    <body style="background-color:lightblue;">  
         <div class="container">
 			<br />
 			
